c853aead419cc349b8b3087497ae23d7b2a39a1c
GingerBeardx <ecryptographer@gmail.com>
2020-10-02 20:20:03 -0600

    Created title and entered basic menu

--- RPGConsoleTutorial/Program.cs
+++ RPGConsoleTutorial/Program.cs
@@ -6,8 +6,39 @@ namespace RPGConsoleTutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MakeTitle();
+            MakeMainMenu();
+            Console.ReadLine();
+        }
+        private static void MakeTitle()
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("*                                                 *");
+            Console.WriteLine("*      ┌─┐┌─┐┌┐┌┌─┐┌─┐┬  ┌─┐  ┌─┐┬─┐┌─┐┬ ┬┬       *");
+            Console.WriteLine("*      │  │ ││││└─┐│ ││  ├┤   │  ├┬┘├─┤││││       *");
+            Console.WriteLine("*      └─┘└─┘┘└┘└─┘└─┘┴─┘└─┘  └─┘┴└─┴ ┴└┴┘┴─┘     *");
+            Console.WriteLine("*                                                 *");
+            Console.WriteLine("***************************************************");
+        }
+        private static void MakeMainMenu()
+        {
+            Console.WriteLine("\n\n(S)tart a new game");
+            Console.WriteLine("(L)Load a saved game");
+            Console.WriteLine("(C)reate a new character");
+            var MainMenuChoice = Console.ReadLine().ToUpper();
 
+            Console.WriteLine($"YOU CHOSE - {MainMenuChoice}");
+
+            if (MainMenuChoice == "S" || MainMenuChoice == "L" || MainMenuChoice == "C")
+            {
+                Console.WriteLine("Thanks!  This choice did nothing!");
+            }
+            else
+            {
+                Console.WriteLine("You have chosen....poorly!");
+            }
         }
     }
 }
